e6f93d7121b9e57eec32d0b597963ceb7903d8bf
Your Name <avrmayuri74@gmail.com>
2023-02-27 00:36:45 -0600

    ADD DAY 1

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello " + input("what is your name ?"))
+
+# input varibales looks for input and then add print statement
+#Write your code below this line 👇
+# Length -- len()--> inside is a input of the length of the string 
+
+print(len(input("What is your name? ")))
+
+# 
