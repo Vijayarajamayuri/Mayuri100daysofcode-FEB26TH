a07bb26bc7235d4860ceded9d86ca456db9f9731
Your Name <avrmayuri74@gmail.com>
2023-03-07 00:15:59 -0600

    Day4

---
+++ Day4/randomisation.py
@@ -0,0 +1,9 @@
+import random
+# importing random module using python random is built-in module where as you can use this technique to add random variables 
+
+# as custom module 
+# import mymodule
+# print(mymodue.pi)
+
+random_integer = random.randint(1,100)
+print(random_integer)
